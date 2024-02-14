e6cc4bcef7451a1debc156bfa61d2221a10c526c
marie-m-c <cherif.mariem.777@gmail.com>
2024-02-14 22:19:16 +0100

    Add first java program assignment

---
+++ fundamentals/first_java_program/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String [] args) {
+        System.out.println("My name is Jane Doe");
+        System.out.println("I am 20 years old");
+        System.out.println("I am from Massachusetts");
+    }
+}
