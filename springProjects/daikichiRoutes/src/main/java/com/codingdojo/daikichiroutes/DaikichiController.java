f6e094866228de0dbfb23e5457da3be0c6a2c6e1
marie-m-c <cherif.mariem.777@gmail.com>
2024-02-24 15:34:49 +0100

    Add daikichi path variables assignment

--- springProjects/daikichiRoutes/src/main/java/com/codingdojo/daikichiroutes/DaikichiController.java
+++ springProjects/daikichiRoutes/src/main/java/com/codingdojo/daikichiroutes/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.codingdojo.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ public class DaikichiController {
     public String welcomeMessage() {
             return "Welcome!";
     }
-	
+
 	@RequestMapping("/today")
     public String todayMessage() {
             return "Today you will find luck in all your endeavors!";
@@ -19,5 +20,19 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow")
     public String tomorrowMessage() {
             return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+
+	@RequestMapping("/travel/{city}")
+    public String travelMessage(@PathVariable("city") String city) {
+        return "Congratulations! You will soon travel to " + city + "!";
+    }
+
+	@RequestMapping("/lotto/{number}")
+    public String lottoMessage(@PathVariable("number") int number) {
+        if (number % 2 == 0) {
+            return "You will take a grand journey in the near future, but be wary of tempting offers.";
+        } else {
+            return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+        }
     }
 }
